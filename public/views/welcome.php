344295a92afe188a0b1b9850a9fbc1a295fe36d5
Callv Group <refkinscallv@gmail.com>
2024-10-08 12:26:58 +0700

    [Version] 1.0.8

--- public/views/welcome.php
+++ public/views/welcome.php
@@ -8,7 +8,7 @@
     <meta name="robots" content="noindex, nofollow">
     <meta name="googlebot" content="noindex, nofollow">
 
-    <title><?= $title ?></title>
+    <title>Philum</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
